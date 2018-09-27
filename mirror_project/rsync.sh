87631a1038290935885b6257dbb5b8ee36b075bf
slmoby <lotusdocker@gmail.com>
2018-09-27 08:44:44 +0800

    update

--- mirror_project/rsync.sh
+++ mirror_project/rsync.sh
@@ -105,7 +105,7 @@ function Rsync_Mongodb_For_Centos6(){
 }
 
 function Rysnc_Mariadb_For_CentOS7(){
-        rsync -avz --exclude-from=$Config_DIR/maradb10.3.list rsync://mirrors.tuna.tsinghua.edu.cn/mariadb/mariadb-10.3.8/yum/centos73-amd64/ $Maridb
+        rsync -avz --exclude-from=$Config_DIR/mariadb10.3.list rsync://mirrors.tuna.tsinghua.edu.cn/mariadb/mariadb-10.3.8/yum/centos73-amd64/ $Maridb
         if [ -d $Mariadb/repodata ];then
             createrepo --update $Mariadb/repodata
             echo "mariadb.10.3.8 for CentOS7"> $MONGODB/index.html
